1a85c90e5a574dc8f8a5791a5100250790e59d06
bilawal0 <mr.bilawal@live.com>
2023-05-31 15:30:04 +0300

    changed api url to production

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,4 +1,8 @@
 export const BASE_URL =
-  'https://helsingborg-transport-visualization-backend-3ca3faqvfa-lz.a.run.app';
+  ' https://helsingborg-transport-visualization-backend-umfrisc3pa-lz.a.run.app';
+
+//Staging url
+// export const BASE_URL =
+//   'https://helsingborg-transport-visualization-backend-3ca3faqvfa-lz.a.run.app';
 export const LOCATION_TASK_NAME = 'LOCATION_TASK_NAME';
 export const LOCATION_SERVICE_CALL_INTERVAL_TIME = 1000 * 60 * 3;
